69832613cc4dc98e556038a2d958f5773ca01490
thainv-vnist <thainv.vnist@gmail.com>
2019-09-13 07:29:12 +0700

    add api register, login for backend

--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-    
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 const app = express();
 
-//Body parser middware
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
-//DB config
-const db = require('./config/key').mongoURI;
+// DB Config
+const db = require("./config/keys").mongoURI;
 
-//Connect to Mongo
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server start on port ${port}`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
