e1aeed399bc6ce415f8f1ff285e93ba80a350a09
BuiVanHuy <huybuivan5797@gmail.com>
2019-09-19 14:14:04 +0700

    update

--- server/controllers/auth/LoginController.js
+++ server/controllers/auth/LoginController.js
@@ -29,69 +29,13 @@ module.exports = function (req, res) {
 
     // Find user by email
     User.findOne({ email })
-<<<<<<< HEAD
-        .populate('id_group')
-=======
         // .populate('id_group')
         .populate([{ path: 'id_group', populate: { path: 'id_role', populate: {path: 'id_permission'} }}])
->>>>>>> 907714e529fcb169d70c7ce56d764a5a26d6bed7
         .then(user => {
         // Check if user exists
         if (!user) {
         return res.status(404).json({ emailnotfound: "Email not found" });
         }
-<<<<<<< HEAD
-        Group.findById(user.id_group)
-        .populate('id_role')
-        .exec((err, group) => {
-            if(!err){
-                Role.findById(group.id_role)
-                    .populate('id_permission')
-                    .exec((err, role) => {
-                        if(!err){
-                            // Check password
-                            bcrypt.compare(password, user.password).then(isMatch => {
-                                if (isMatch) {
-                                    // User matched
-                                    // Create JWT Payload
-                                    const payload = {
-                                        id: user.id,
-                                        name: user.name
-                                    };
-
-                                    // Sign token
-                                    jwt.sign(
-                                        payload,
-                                        keys.secretOrKey,
-                                        {
-                                            expiresIn: 31556926 // 1 year in seconds
-                                        },
-                                        (err, token) => {
-                                            res.json({
-                                            success: true,
-                                            token: "VNIST " + token,
-                                            user: {
-                                                    "name": user.name,
-                                                    "email": user.email,
-                                                    "group": {
-                                                        group_info: user.id_group,
-                                                        role: role
-                                                    }
-                                                }
-                                            });
-                                        }
-                                    );
-                                } else {
-                                    return res
-                                        .status(400)
-                                        .json({ passwordincorrect: "Password incorrect" });
-                                }
-                            });
-                        }
-                    })
-            }
-        })
-=======
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
                 // User matched
@@ -137,7 +81,6 @@ module.exports = function (req, res) {
         //             })
         //     }
         // })
->>>>>>> 907714e529fcb169d70c7ce56d764a5a26d6bed7
     });
 };
 
