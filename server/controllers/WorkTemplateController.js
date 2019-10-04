03533f5792f2221ed1769c3c1e920e4c3f5bf5d7
Thai Nguyen Viet <thainv.vnist@gmail.com>
2019-10-04 17:38:51 +0700

    API getByID , delete of WorkTemplate

--- server/controllers/WorkTemplateController.js
+++ server/controllers/WorkTemplateController.js
@@ -2,11 +2,20 @@ const WorkTemplateService = require('../services/WorkTemplate.service');
 
 exports.get = (req, res) => {
     return WorkTemplateService.get(req, res);
-};
+}
+
+exports.getById = (req, res) => {
+    return WorkTemplateService.getById(req, res);
+}
+
+exports.getByJobTitle = (req, res) => {
+    return WorkTemplateService.getByJobTitle(req, res);
+}
 
 exports.create = (req, res) => {
     return WorkTemplateService.create(req, res);
 }
-exports.getByJobTitle = (req, res) => {
-    return WorkTemplateService.getByJobTitle(req, res);
+
+exports.delete = (req, res) => {
+    return WorkTemplateService.delete(req, res);
 }
